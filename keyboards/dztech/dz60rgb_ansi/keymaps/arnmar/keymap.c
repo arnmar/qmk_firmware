7c190e25fffc416b0de7fb7018307c84867bdb9d
Marcus <m@m>
2021-12-05 21:11:17 +0100

    More changes

--- keyboards/dztech/dz60rgb_ansi/keymaps/arnmar/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/arnmar/keymap.c
@@ -28,23 +28,25 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 #define SPC     LT(_NAV, KC_SPACE)
 #define CAPS    LT(_NAV, KC_ESCAPE)
+#define TAB     LT(_HDUE, KC_TAB)
 #define KEY_D   LSFT_T(KC_D)
 #define KEY_F   LCTL_T(KC_F)
 #define KEY_J   RCTL_T(KC_J)
 #define KEY_K   RSFT_T(KC_K)
+#define L_ALT   MT(MOD_LALT, KC_QUOTE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_60_ansi(
-        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, TD(TD_SWE1), KC_RBRC, KC_BSLS,
-        CAPS,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD(TD_SWE3), TD(TD_SWE2),  KC_ENT,
-        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-        KC_LCTL,   KC_LGUI, KC_LALT,                            SPC,                                KC_RALT, MO(_NAV), MO(_RGB), KC_RCTL
+        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,
+        TAB,       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,
+        CAPS,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,       KC_ENT,
+        KC_LSPO,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,       KC_RSPC,
+        KC_LCTL,   KC_LGUI, L_ALT,                            SPC,                                KC_RALT, MO(_NAV), MO(_RGB), KC_RCTL
     ),
     [_NAV] = LAYOUT_60_ansi(
         KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______, _______, WORD_FW, _______, _______, _______, _______, _______, _______, _______, _______, RALT(KC_W), _______, RESET,
-        _______, MO(_HDUE), _______, _______, _______, _______, KC_LEFT, KC_DOWN,  KC_UP, KC_RIGHT, RALT(KC_P), RALT(KC_Q),  _______,
+        MO(_RGB), _______, WORD_FW, _______, _______, _______, _______, _______, _______, _______, _______, RALT(KC_W), _______, RESET,
+        _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,  KC_UP, KC_RIGHT, RALT(KC_P), RALT(KC_Q),  _______,
         _______,          _______, _______, _______, _______, WORD_BK, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, _______
     ),
