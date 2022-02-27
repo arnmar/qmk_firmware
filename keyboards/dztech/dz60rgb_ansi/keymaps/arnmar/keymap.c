a61d0824423c1c3a294fea0ef1a5231dab2ad638
Marcus <m@m>
2022-02-27 12:33:07 +0100

    More modifications

--- keyboards/dztech/dz60rgb_ansi/keymaps/arnmar/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/arnmar/keymap.c
@@ -6,6 +6,7 @@ enum dz60rgb_layers {
     _HDUE,
     _SYM,
     _RGB,
+    _GAME,
 }; 
 
 enum custom_keycodes {
@@ -79,8 +80,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______,  KC_GRV,S(KC_GRV), DOUBLE_COLON, ARROW, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, _______, _______, _______,
         _______, _______, _______, KC_BSPC,  KC_ENT, _______, KC_LEFT, KC_DOWN,  KC_UP, KC_RIGHT, _______, _______,  _______,
-        _______,          _______, _______, _______, _______, _______, _______, KC_BSPC,  KC_DEL, _______, _______,          _______,
-        _______, _______, _______,                              _______,                              _______, _______, _______, _______
+        _______,          _______, _______, _______, _______, KC_SPACE, _______, KC_BSPC,  KC_DEL, _______, _______,          _______,
+        _______, _______, _______,                            _______,                            _______, DF(_GAME), _______, _______
     ),
     [_RGB] = LAYOUT_60_ansi(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
@@ -88,7 +89,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, RGB_RMOD, RGB_MOD, _______, _______, _______, _______, _______, RGB_SPI, RGB_SPD, _______, _______,          _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, _______
-    )
+    ),
+    [_GAME] = LAYOUT_60_ansi(
+        KC_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+        CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
+        KC_LCTL,        KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, DF(_BASE),   MO(2),   KC_RCTL
+    ),
 }; 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
