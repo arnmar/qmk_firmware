e7aa03d312556cd6b4d776b2ce7522c1b05f9d75
Marcus <m@m>
2022-10-22 08:31:20 +0200

    Reverted back to EUR key layout

--- keyboards/dz60/keymaps/arnmar/keymap.c
+++ keyboards/dz60/keymaps/arnmar/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD(TD_SWE1), KC_RBRC, KC_BSPC,
 		CAPS,             KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD(TD_SWE3), TD(TD_SWE2), KC_ENT,
 		KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,              KC_RSFT, MO(_RGB),
-		              _LGUI,             KC_LALT,                      SPC,                      KC_RALT,           MO(_SYM)),
+		            KC_LGUI,             KC_LALT,                      SPC,                      KC_RALT,           MO(_SYM)),
 	[_NAV] = LAYOUT_60_hhkb(
 		KC_GRV, KC_F1,   KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,  KC_F11,   KC_F12,    KC_INS,   RESET,
         _______, _______, WORD_FW, _______, _______, _______,    YANK,   KC_BSPC,   KC_INS,  _______,    PASTE, _______,  _______,   	KC_DEL,
