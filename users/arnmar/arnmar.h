16cb986aedc1fe6270e32acf3e3f7950788fb77a
Marcus Arnleve <m@m>
2026-03-13 15:34:54 +0100

    Updates to keymap

--- users/arnmar/arnmar.h
+++ users/arnmar/arnmar.h
@@ -40,24 +40,40 @@ enum {
 #define CAPS    MT(MOD_LCTL, KC_ESC)
 #define TAB     LT(_HDUE, KC_TAB)
 #define _LGUI   LT(_NUM, KC_LGUI)
-// Home row mods
-#define K_A     LGUI_T(KC_A)
-#define K_S     LALT_T(KC_S)
-#define K_D     LSFT_T(KC_D)
-#define K_F     LCTL_T(KC_F)
-#define K_J     RCTL_T(KC_J)
-#define K_K     RSFT_T(KC_K)
-#define K_L     RALT_T(KC_L)
-// Bottom row mods
-#define K_Z     LT(_SYM, KC_Z)
-#define K_X     LALT_T(KC_X)
-#define K_C     LSFT_T(KC_C)
-#define K_V     LCTL_T(KC_V)
-#define K_M     RCTL_T(KC_M)
-#define K_COMM  RSFT_T(KC_COMM)
-#define K_DOT   RALT_T(KC_DOT)
+
+#define K_Q     KC_Q
+#define K_W     KC_W
+#define K_E     LSFT_T(KC_E)
+#define K_R     LCTL_T(KC_R)
+#define K_T     KC_T
+#define K_Y     KC_Y
+#define K_U     RCTL_T(KC_U)
+#define K_I     RSFT_T(KC_I)
+#define K_O     KC_O
+#define K_P     KC_P
+
+#define K_A     LT(_SYM, KC_A)
+#define K_S     KC_S
+#define K_D     KC_D
+#define K_F     KC_F
+#define K_G     KC_G
+#define K_H     KC_H
+#define K_J     KC_J
+#define K_K     KC_K
+#define K_L     KC_L
+
+#define K_Z     KC_Z
+#define K_X     KC_X
+#define K_C     KC_C
+#define K_V     KC_V
+#define K_B     KC_B
+#define K_N     KC_N
+#define K_M     KC_M
+#define K_COMM  KC_COMM
+#define K_DOT   KC_DOT
 #define L_ALT   MT(MOD_LALT, KC_QUOTE)
 #define K_SLSH  LT(_SYM, KC_SLSH)
+
 #define SWE1    TD(TD_SWE1)
 #define SWE2    TD(TD_SWE2)
 #define SWE3    TD(TD_SWE3)
